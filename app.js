1c0b00593906431ca87da4c9276de7f8924722b5
ABANISH <avnish97090@gmail.com>
2023-07-15 09:20:19 +0530

    up to third episode

--- app.js
+++ app.js
@@ -6,7 +6,46 @@ const heading3 = React.createElement("h3",{}, "namaste react 3");
 const heading4= React.createElement("h1" , {} , "namstey react4");
 
 const container = React.createElement("div", {id : "container"} , [heading1, heading2 ,heading3,heading4]);
+
+ const heading = (
+    <h1 id = "title" key = " h2" >
+        Namaste React 
+    </h1>
+ );
+
+//  const HeaderComponent = () => {
+//     return (
+//         <div>
+//             <h2> Namaste React functional component</h2>
+//             <h2> this is a tag h2</h2>
+//         </div>
+//     );
+
+//  };
+ var xyz = 10;
+const HeaderComponent = () => {
+    return (
+        <div>
+           
+            {xyz}  {/*whenever we write jsx  we can write any piece of javascript code in { } */}
+            {heading}
+            <h2> Namaste React functional component</h2>
+            <h2> this is a tag h2</h2>
+        </div>
+    );
+
+ };
+
 const root = ReactDOM.createRoot(
    
     document.getElementById("root"));
-    root.render(container);
+    // root.render(container); 
+
+    // root.render(heading);
+    root.render(<HeaderComponent/>)
+
+
+
+
+
+
